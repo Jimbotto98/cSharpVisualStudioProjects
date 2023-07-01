a55f42bbe6041b895662a33c4d69a2794657664c
James Cardona <133485509+Jimbotto98@users.noreply.github.com>
2023-07-01 19:54:50 +0700

    Add files via upload

---
+++ superSimpleCalculator1.cs
@@ -0,0 +1,42 @@
+﻿namespace superSimpleCalculator.cs
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // My superSimpleCalculator
+
+            //The superSimpleCalculator reads two integers from the user
+            //& adds them together.
+
+                //Console.WriteLine("enter a number");moved it
+                //Console.ReadLine(); My initial code. I moved it later
+
+                //Console.WriteLine("enter another number"); moved it
+                //Console.ReadLine(); Same thing.
+
+            //so far I have a WriteLine and ReadLine, now to make it work
+
+            Console.WriteLine("enter a number");
+
+                //int spaceOne = Console.ReadLine();My initial code.
+            int firstInt = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter another number");
+
+            int secondInt = int.Parse(Console.ReadLine());
+
+            //I knew I needed to put int. Now I just learned about Parse
+
+            int answer = firstInt + secondInt;
+            //I was happy I got that right
+
+            Console.WriteLine("voila");
+
+            Console.WriteLine(answer);
+
+            
+        }
+    }
+}
+
